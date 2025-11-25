1912287b7e1dbfd16b662a238a1822069efbd7a9
gvbvdxxalt2 <jgevans@students.wcpss.net>
2025-11-25 13:12:23 -0500

    Commit

--- src/interface/dom/index.js
+++ src/interface/dom/index.js
@@ -21,6 +21,7 @@ elements.appendElementsFromJSON(document.body, [
           require("./right.js"), //Right panel
         ],
       },
+      require("./loading.js")
     ],
   },
 ]);
---
+++ src/interface/dom/loading.js
@@ -0,0 +1,27 @@
+module.exports = {
+    element: "div",
+    gid: "loadingScreenContainer",
+    children: [
+        {
+            element: "div",
+            className: "loadingDialogBG"
+        },
+        {
+            element: "div",
+            className: "loadingDialogBox",
+            children: [
+                {
+                    element: "div",
+                    className: "loader2"
+                },
+                {
+                    element: "span",
+                    style: {
+                        fontSize: "30px"
+                    },
+                    textContent: "Loading..."
+                }
+            ]
+        }
+    ]
+};
